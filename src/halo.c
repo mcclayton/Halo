e7e9cda536acbd033e0103e82ec7d8ca7391e70e
Michael Clayton <mostly_mike@hotmail.com>
2014-05-14 12:06:38 -0400

    Added date text.

--- src/halo.c
+++ src/halo.c
@@ -3,9 +3,11 @@
 Window *window;
 TextLayer *text_hour_layer;
 TextLayer *text_minute_layer;
+TextLayer *text_date_layer;
 Layer *minute_circle_layer;
 GFont dosis_font_56;
 GFont dosis_font_18;
+GFont dosis_font_16;
 
 #define BACKGROUND GColorBlack
 #define FOREGROUND GColorWhite
@@ -43,10 +45,10 @@ void circle_layer_update_callback(Layer *layer, GContext* ctx) {
 	}
 
 	// Draw divider between hour and minutes text
-	const GPoint dividerStartOne = ((GPoint) {(bounds.size.w/2)-22, (bounds.size.h/2)+19});
-	const GPoint dividerEndOne = ((GPoint) {(bounds.size.w/2)+22, (bounds.size.h/2)+19});
-	const GPoint dividerStartTwo = ((GPoint) {(bounds.size.w/2)-20, (bounds.size.h/2)+21});
-	const GPoint dividerEndTwo = ((GPoint) {(bounds.size.w/2)+20, (bounds.size.h/2)+21});
+	const GPoint dividerStartOne = ((GPoint) {(bounds.size.w/2)-22, (bounds.size.h/2)+17});
+	const GPoint dividerEndOne = ((GPoint) {(bounds.size.w/2)+22, (bounds.size.h/2)+17});
+	const GPoint dividerStartTwo = ((GPoint) {(bounds.size.w/2)-22, (bounds.size.h/2)+18});
+	const GPoint dividerEndTwo = ((GPoint) {(bounds.size.w/2)+22, (bounds.size.h/2)+18});
 
 	graphics_context_set_stroke_color(ctx, FOREGROUND);
 	graphics_draw_line(ctx, dividerStartOne, dividerEndOne);
@@ -57,6 +59,7 @@ void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 	// Need to be static because they're used by the system later.
 	static char time_text[] = "00";
 	static char minute_text[] = "00";
+	static char date_text[32];
 
 	char *time_format;
 
@@ -84,16 +87,24 @@ void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 
 	// Update the minutes text
 	time_format = "%M";
-	strftime(minute_text, sizeof(time_text), time_format, tick_time);
+	strftime(minute_text, sizeof(minute_text), time_format, tick_time);
 	text_layer_set_text(text_minute_layer, minute_text);
+
+	// Update the date text
+	time_format = "%b %d";
+	strftime(date_text, sizeof(date_text), time_format, tick_time);
+	text_layer_set_text(text_date_layer, date_text);
 }
 
 void handle_deinit(void) {
 	tick_timer_service_unsubscribe();
 	text_layer_destroy(text_hour_layer);
+	text_layer_destroy(text_minute_layer);
+	text_layer_destroy(text_date_layer);
 	layer_destroy(minute_circle_layer);
 	fonts_unload_custom_font(dosis_font_56);
 	fonts_unload_custom_font(dosis_font_18);
+	fonts_unload_custom_font(dosis_font_16);
 	window_destroy(window);
 }
 
@@ -111,6 +122,7 @@ void handle_init(void) {
 	// Load in the fonts
 	dosis_font_56 = fonts_load_custom_font( resource_get_handle(RESOURCE_ID_FONT_DOSIS_56) );
 	dosis_font_18 = fonts_load_custom_font( resource_get_handle(RESOURCE_ID_FONT_DOSIS_18) );
+	dosis_font_16 = fonts_load_custom_font( resource_get_handle(RESOURCE_ID_FONT_DOSIS_16) );
 
 	GRect full_window_frame = ((GRect) { .origin = { 0, 0 }, .size = { window_bounds.size.w, window_bounds.size.h } });
 	minute_circle_layer = layer_create(full_window_frame);
@@ -119,10 +131,13 @@ void handle_init(void) {
 
 
 	// Create a text layer in the center of the screen for the hour
-	text_hour_layer = text_layer_create((GRect) { .origin = { 0, 40 }, .size = { window_bounds.size.w, window_bounds.size.h/2 } });
+	text_hour_layer = text_layer_create((GRect) { .origin = { 0, 37 }, .size = { window_bounds.size.w, window_bounds.size.h/2 } });
 
 	// Create a text layer in the center of the screen for minutes (shift origin to be below hourly text)
-	text_minute_layer = text_layer_create((GRect) { .origin = { 0, 105 }, .size = { window_bounds.size.w, window_bounds.size.h/2 } });
+	text_minute_layer = text_layer_create((GRect) { .origin = { 0, 103 }, .size = { window_bounds.size.w, window_bounds.size.h/2 } });
+
+	// Create a text layer in the center of the screen for the date (shift origin to be at the bottome of screen text)
+	text_date_layer = text_layer_create((GRect) { .origin = { 0, 149 }, .size = { window_bounds.size.w, window_bounds.size.h/2 } });
 
 	// Set the color/font/alignment of hourly text
 	text_layer_set_background_color(text_hour_layer, GColorClear);
@@ -137,6 +152,13 @@ void handle_init(void) {
 	text_layer_set_text_color(text_minute_layer, FOREGROUND);
 	text_layer_set_text_alignment(text_minute_layer, GTextAlignmentCenter);
 	layer_add_child(window_layer, text_layer_get_layer(text_minute_layer));
+
+	// Set the color/font/alignment of date text
+	text_layer_set_background_color(text_date_layer, GColorClear);
+	text_layer_set_font(text_date_layer, dosis_font_16);
+	text_layer_set_text_color(text_date_layer, FOREGROUND);
+	text_layer_set_text_alignment(text_date_layer, GTextAlignmentCenter);
+	layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
 
 	// Subscribe to the timer service
 	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
